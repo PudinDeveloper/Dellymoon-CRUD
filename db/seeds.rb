196dfd521647ed288202905629b27ef9a6e532a3
PudinDeveloper <diegoalejandropolanco@gmail.com>
2024-03-24 12:13:44 -0600

    Create food seeds in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# Create foods
+20.times do
+  # Create a food record in the database.
+  Food.create(
+    name: Faker::Food.dish,
+    quantity: Faker::Number.between(from: 1, to: 10),
+    price: Faker::Commerce.price(range: 5.0..50.0),
+    food_type: ['Main dish', 'Beverage'].sample
+  )
+end
+
+# Output a success message.
+puts "Foods seeded successfully!"

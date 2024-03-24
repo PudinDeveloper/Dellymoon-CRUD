faed701a8c1a8b28de742df63b8f3c5d3d1b78eb
PudinDeveloper <diegoalejandropolanco@gmail.com>
2024-03-24 14:04:48 -0600

    Fix Soft delete button on foods index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_24_061754) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_24_192124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_24_061754) do
     t.string "food_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "isActive", default: true, null: false
   end
 
   create_table "order_foods", force: :cascade do |t|

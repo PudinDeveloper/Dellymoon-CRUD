16587e9fa45915e18c6cfc9deef4c5eb6adcc7b8
PudinDeveloper <diegoalejandropolanco@gmail.com>
2024-03-24 13:02:55 -0600

    Modify the seeds.rb file to add correct data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+# Delte all the records in the foods table.
+Food.destroy_all
+puts "Foods destroyed successfully!"
 
 # Create foods
-20.times do
+7.times do
   # Create a food record in the database.
   Food.create(
     name: Faker::Food.dish,
-    quantity: Faker::Number.between(from: 1, to: 10),
+    quantity: Faker::Number.between(from: 100.0, to: 450.0),
     price: Faker::Commerce.price(range: 5.0..50.0),
-    food_type: ['Main dish', 'Beverage'].sample
+    food_type: 'Main dish'
+  )
+
+  # Create a beverage record in the database.
+  Food.create(
+    name: Faker::Beer.name,
+    quantity: Faker::Number.between(from: 100.0, to: 450.0),
+    price: Faker::Commerce.price(range: 5.0..50.0),
+    food_type: 'Beverage'
+  )
+
+  # Create a dessert record in the database.
+  Food.create(
+    name: Faker::Dessert.variety,
+    quantity: Faker::Number.between(from: 100.0, to: 450.0),
+    price: Faker::Commerce.price(range: 5.0..50.0),
+    food_type: 'Dessert'
   )
 end
 

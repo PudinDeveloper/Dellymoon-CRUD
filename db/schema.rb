409f6d7a65ea4a25233d4f055fecf23ac8a61b16
PudinDeveloper <diegoalejandropolanco@gmail.com>
2024-03-24 11:02:14 -0600

    Change the database.yml file to use PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_24_061754) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"

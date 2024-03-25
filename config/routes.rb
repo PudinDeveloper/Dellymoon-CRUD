66ffd8d972cac68a8a8b2e8ac547cb4c092ea183
PudinDeveloper <diegoalejandropolanco@gmail.com>
2024-03-24 19:57:06 -0600

    Refactor of routes and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  # /admin redirect to root
+  get '/admin' => redirect('/')
+  get '/admin/login' => redirect('/')
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   resources :order_foods
   resources :tickets
   resources :foods
   devise_for :users
+
   get 'main/home'
   root "main#home"
 end

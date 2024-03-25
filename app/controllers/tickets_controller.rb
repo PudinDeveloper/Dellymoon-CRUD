356d615cfa6107db0824b7b0351b6e4c4d383a1f
PudinDeveloper <diegoalejandropolanco@gmail.com>
2024-03-24 18:57:50 -0600

    Refactor tickets_controller and add documentation

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -11,11 +11,16 @@ class TicketsController < ApplicationController
 
   # GET /tickets/1 or /tickets/1.json
   def show
+    # This code is for displaying the ordered foods in the ticket show page
     @employee = User.find_by(id: (@ticket.user_id))
+
+    # If the employee exists, then the orders are displayed based on the ticket id
     if @employee
       @orders = OrderFood.where(ticket_id: params[:id])
     else
-      flash[:alert] = 'Ocurrió un error, no se encontro el empleado, revisa la integridad de la base de datos'
+      # If the employee does not exist, then the user is redirected to the tickets page
+      # This error does not suppose to happen, but it is a security measure
+      flash[:alert] = 'Something went wrong, the employee was not found, check the database integrity'
       redirect_to tickets_path
     end
   end
@@ -34,20 +39,26 @@ class TicketsController < ApplicationController
 
   # POST /tickets or /tickets.json
   def create
+
     @find_employee = User.find_by(id: (current_user.id).to_i)
+
     if @find_employee
+      # Create a new ticket with the table number and the user id
+      # table: params[:ticket][:table] is the table number that the user inputs in the form
+      # user_id: current_user.id is the user id of the user that is currently logged in
       @ticket = Ticket.new(table: params[:ticket][:table], user_id: current_user.id)
-      respond_to do |format|
-        if @ticket.save
-          format.html { redirect_to ticket_url(@ticket), notice: "Ticket was successfully created." }
-          format.json { render :show, status: :created, location: @ticket }
-        else
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @ticket.errors, status: :unprocessable_entity }
-        end
+
+      if @ticket.save
+        flash[:notice] = "Ticket was successfully created."
+        redirect_to ticket_url(@ticket)
+      else
+        render :new, status: :unprocessable_entity
       end
+
+    # If the employee does not exist, then the user is redirected to the new ticket page
+    # This error does not suppose to happen, but it is a security measure
     else
-      flash[:alert] = 'Ese empleado no existe, usa el ID: "1" si no cuentas con ID de empleado'
+      flash[:alert] = 'Employee does not exist, please contact the system administrator'
       redirect_to new_ticket_url
     end
 
@@ -56,42 +67,35 @@ class TicketsController < ApplicationController
   # PATCH/PUT /tickets/1 or /tickets/1.json
   def update
 
-    # byebug
-
+    # The reason for this is that the update method is used for both updating the ticket and adding a dish to the ticket
     OrderFood.create(food_id: params[:dish], ticket_id: params[:id])
-    update_final_price
-    redirect_to ticket_url(params[:id]), notice: "Ticket was successfully updated."
 
-    # respond_to do |format|
-    #   if @ticket.update(ticket_params)
-    #     format.html { redirect_to ticket_url(@ticket), notice: "Ticket was successfully updated." }
-    #     format.json { render :show, status: :ok, location: @ticket }
-    #   else
-    #     format.html { render :edit, status: :unprocessable_entity }
-    #     format.json { render json: @ticket.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    # This code is for updating the final price of the ticket
+    update_final_price
+
+    # Finally, redirect to the ticket show page
+    flash[:notice] = "Ticket was successfully updated."
+    redirect_to ticket_url(params[:id])
+
   end
 
   # DELETE /tickets/1 or /tickets/1.json
   def destroy
+
+    # If the dish parameter exists, then the dish is removed from the ticket
     if params[:dish]
       OrderFood.find(params[:dish]).destroy
       update_final_price
-      redirect_to ticket_url(params[:id]), notice: "Dish was successfully removed."
+      flash[:notice] = "Dish was successfully removed."
+      redirect_to ticket_url(params[:id])
     else
       @ticket_temp = Ticket.find(params[:id])
       @ticket_temp.status = false
       @ticket_temp.save
-      redirect_to tickets_url, notice: "Ticket ha sido marcado como inactivo."
+      flash[:notice] = "Ticket was successfully marked as inactive."
+      redirect_to tickets_url
     end
 
-    # @ticket.destroy
-
-    # respond_to do |format|
-    #   format.html { redirect_to tickets_url, notice: "Ticket was successfully destroyed." }
-    #   format.json { head :no_content }
-    # end
   end
 
   private
@@ -106,20 +110,27 @@ class TicketsController < ApplicationController
     end
 
     def update_final_price
+      # Updates the final price of a ticket based on the prices of the ordered foods.
       @final_price = OrderFood.where(ticket_id: params[:id])
+
       @fp = 0
+
       @final_price.each do |price|
         @food = Food.find(price.food_id)
         @fp += @food.price
       end
+
+      # This method calculates the final price of a ticket by summing up the prices of all the ordered foods associated with the ticket.
+      # It retrieves the ordered foods from the `OrderFood` model and finds the corresponding `Food` model to get the price.
+      # The calculated final price is then assigned to the `final_price` attribute of the `Ticket` model and saved.
       @lticket = Ticket.find(params[:id])
       @lticket.final_price = @fp
       @lticket.save
     end
 
     def authorize_role
-      unless current_user && (current_user.role == "Mesero" || current_user.role == "Admin")
-        flash[:alert] = "Acceso denegado."
+      unless current_user && (current_user.role == "Waitstaff" || current_user.role == "Admin")
+        flash[:alert] = "Your role '#{current_user.role}' is not authorized to access this page."
         redirect_to root_path
       end
     end

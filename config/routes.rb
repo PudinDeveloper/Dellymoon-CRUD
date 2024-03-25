3c55511b426d51f4a744687b52a99b7268075b63
PudinDeveloper <diegoalejandropolanco@gmail.com>
2024-03-25 09:31:20 -0600

    Remove registration link from login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # /admin redirect
   get '/admin' => redirect('/admin_panel')
   get '/admin/login' => redirect('/admin_panel')
+  get '/users/sign_up' => redirect('/users/sign_in')
 
   resources :order_foods
   resources :tickets

2b722b10cda453e110a78b36c1331049aa2c2892
PudinDeveloper <diegoalejandropolanco@gmail.com>
2024-03-25 10:52:39 -0600

    Add food model validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,6 +4,12 @@ class Food < ApplicationRecord
   validates :price, presence: true
   validates :food_type, presence: true
 
+  # Validate that price is a number and greater or equal to 0.
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  # Validate that quantity is a number and greater than 0.
+  validates :quantity, numericality: { greater_than: 0 }
+
   # This method returns a list of food types.
   def self.food_types
     ['Main dish', 'Drink', 'Dessert']
